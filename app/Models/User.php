43b73a6b336d6799c302fd88c664c8a767457f90
vim <9wimu9@gmail.com>
2022-09-25 20:07:05 +0530

    add role

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,6 +50,10 @@ class User extends Authenticatable implements JWTSubject
 
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'id' => $this->id,
+            'role' => $this->role,
+        ];
+
     }
 }
